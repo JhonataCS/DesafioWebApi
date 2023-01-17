78039faee2ae1789eaf261111086ce5fb201b963
Jhonata Silva <jhonata.silva@bs2tecnologia.com.br>
2023-01-17 16:10:49 -0300

    migrationSql

--- Migrations/20230117185149_ConfigDB.Designer.cs
+++ Migrations/20230117185149_ConfigDB.Designer.cs
@@ -4,6 +4,7 @@ using DesafioWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DesafioWebApi.Migrations
 {
     [DbContext(typeof(ContextoDB))]
-    partial class ContextoDBModelSnapshot : ModelSnapshot
+    [Migration("20230117185149_ConfigDB")]
+    partial class ConfigDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
